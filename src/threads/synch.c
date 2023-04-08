1b5e1d8523f96dd8fcf5ace86072a5aad2ebdff8
seonkyuKim <hopsprings2ternal@gmail.com>
2023-04-08 11:05:02 +0900

    에러수정

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -70,7 +70,7 @@ sema_down (struct semaphore *sema)
     {
       // If new thread comes, compare the priority between waiters.
       // Do donation if waiting thread's priority is lower than the current thread's priority.
-      struct list_elem e;
+      struct list_elem* e;
       for (e = list_begin(&sema->waiters); e != list_end(&sema->waiters);
        e = list_next(e));
       {
